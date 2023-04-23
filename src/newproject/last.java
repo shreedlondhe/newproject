8d4a162f1cbbd17227a255732a005695cbfa8ac7
shreelondhe <shreedlondhe@gmail.com>
2023-04-23 14:48:48 +0530

    commited

---
+++ src/newproject/last.java
@@ -0,0 +1,16 @@
+package newproject;
+
+public class last {
+	public static void main(String[] args) {
+		int row=5;
+		for(int i=0;i<=5;i++)
+		{
+		for(int j=0;j<=i;j++)	
+		{
+			System.out.print("*");
+		}
+		System.out.println("");
+		}
+
+}
+}
