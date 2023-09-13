e90a67a2e6a59f5830adaeb19e599b292d210144
shreelondhe <shreedlondhe@gmail.com>
2023-09-13 18:54:32 +0530

    deed

---
+++ src/newproject/last.java
@@ -0,0 +1,13 @@
+package newproject;
+
+public class last {
+
+
+	public static void main(String[] args) {
+		System.out.println("print this is ");
+			System.out.println("print this is ");
+		System.out.println("print this is ");
+			
+
+}
+}
