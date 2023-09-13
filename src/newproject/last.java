f72fd493cc0bd6f229be292ead9c5a5a6e8d24da
shreelondhe <shreedlondhe@gmail.com>
2023-09-13 18:56:57 +0530

    second commit

--- src/newproject/last.java
+++ src/newproject/last.java
@@ -6,7 +6,7 @@ public class last {
 	public static void main(String[] args) {
 		System.out.println("print this is ");
 			System.out.println("print this is ");
-		System.out.println("print this is ");
+	
 			
 
 }
